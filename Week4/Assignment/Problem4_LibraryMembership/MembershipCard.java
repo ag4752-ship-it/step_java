043665f6e85d69f3069d0e66da792ce344cff2c9
Anjel <ag4752@srmist.edu.in>
2026-09-04 22:42:00 +0530

    coded week 4 assignment and practice

---
+++ Week4/Assignment/Problem4_LibraryMembership/MembershipCard.java
@@ -0,0 +1,31 @@
+public class MembershipCard {
+    public static String libraryName;
+    public static String validUntil;
+    private String studentName;
+
+    static {
+        libraryName = "SRM Central Library";
+        validUntil = "May 2027";
+        System.out.println("Library info loaded");
+    }
+
+    public MembershipCard(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Ananya", "Rohan", "Priya", "Arjun", "Sneha"};
+
+        for (String name : names) {
+            MembershipCard card = new MembershipCard(name);
+            System.out.println("Membership card issued: " + card.getStudentName());
+        }
+    }
+}
+
+
+
