c027f73a881fcc173784eda5bec0bf41febde5bf
Anjel <ag4752@srmist.edu.in>
2026-09-04 22:42:00 +0530

    coded week 1 assignment and practice

---
+++ Week1/Assignment/Problem3_TrafficSignalStreakAnalyzer/TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,47 @@
+public class TrafficSignalStreakAnalyzer {
+
+    public static void findLongestStreak(String signalLog) {
+        if (signalLog == null || signalLog.isEmpty()) {
+            System.out.println("Empty signal log.");
+            return;
+        }
+
+        char longestColor = signalLog.charAt(0);
+        int maxStreak = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int currentStreak = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+            char c = signalLog.charAt(i);
+            if (c == currentColor) {
+                currentStreak++;
+            } else {
+                if (currentStreak > maxStreak) {
+                    maxStreak = currentStreak;
+                    longestColor = currentColor;
+                }
+                currentColor = c;
+                currentStreak = 1;
+            }
+        }
+
+        if (currentStreak > maxStreak) {
+            maxStreak = currentStreak;
+            longestColor = currentColor;
+        }
+
+        System.out.println("Longest Streak: '" + longestColor + "' repeated " + maxStreak + " times");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--- Test Case 1 ---");
+        findLongestStreak("RRGGGYRR");
+
+        System.out.println("\n--- Test Case 2 ---");
+        findLongestStreak("RRRRYYGG");
+    }
+}
+
+
+
